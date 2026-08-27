d925f84b3063329eec196cdc26e003885f0f11a3
nuwanudayakumara00-hue <nuwanudayakumara00@gmail.com>
2026-08-27 11:20:21 +0530

    Fix responsive navbar for mobile screens

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import Link from "link";
+import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
 import { Rocket, Menu, X } from "lucide-react";
 
